b046372666a7248b891938222f78ebc9ae3ab549
My EVM <yo@myevm.org>
2025-03-22 04:28:33 +0000

    .

---
+++ TWcontracts/contracts/infra/forwarder/ForwarderEOAOnly.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.11;
+
+
+
+import "../../external-deps/openzeppelin/metatx/MinimalForwarderEOAOnly.sol";
+
+/*
+ * @dev Minimal forwarder for GSNv2
+ */
+contract ForwarderEOAOnly is MinimalForwarderEOAOnly {
+    // solhint-disable-next-line no-empty-blocks
+    constructor() MinimalForwarderEOAOnly() {}
+}
