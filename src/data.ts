9015dd2fc800a6615730fa52d5ea2d2e60a02839
My EVM <yo@myevm.org>
2025-03-22 05:39:24 +0000

    .

--- src/data.ts
+++ src/data.ts
@@ -2,19 +2,19 @@
 
 export const articleData = [
     {
-      title: "Coming Soon",
+      title: "Apps",
       href: "https://portal.thirdweb.com/typescript/v5",
       description: "Soon",
       image: "https://cryptologos.cc/logos/ethereum-eth-logo.png",
     },
     {
-      title: "Coming Soon",
+      title: "Apps",
       href: "https://portal.thirdweb.com/typescript/v5/react",
       description: "Soon",
       image: "https://cryptologos.cc/logos/ethereum-eth-logo.png",
     },
     {
-      title: "Coming Soon",
+      title: "Apps",
       href: "https://thirdweb.com/dashboard",
       description: "Soon",
       image: "https://cryptologos.cc/logos/ethereum-eth-logo.png",
