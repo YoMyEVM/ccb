9bfd7c2da0b7f6d799ae5ac5337677f4a934dde9
My EVM <yo@myevm.org>
2025-03-22 03:18:13 +0000

    .

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ export function App() {
     <>
       <Navbar />
       <main className="min-h-[100vh] flex flex-col items-center justify-start container max-w-screen-lg mx-auto">
-        <div className="py-20">
+        <div className="py-10">
           <Header />
           <ThirdwebResources />
         </div>
@@ -20,24 +20,13 @@ export function App() {
 function Header() {
   return (
     <header className="flex flex-col items-center mb-20 md:mb-20">
-      <img
-        src={thirdwebIcon}
-        alt=""
-        className="size-[150px] md:size-[150px]"
-        style={{ filter: "drop-shadow(0px 0px 24px #a726a9a8)" }}
-      />
-      <h1 className="text-2xl md:text-6xl font-bold tracking-tighter mb-6 text-zinc-100">
-        thirdweb SDK
-        <span className="text-zinc-300 inline-block mx-1"> + </span>
-        <span className="inline-block -skew-x-6 text-violet-500"> vite </span>
+
+      <h1 className="text-2xl md:text-6xl font-bold tracking-tighter mt-8 -mb-6 text-zinc-100">
+        My EVM
+		<span className="text-zinc-300 inline-block mx-1">  </span>
+        <span className="inline-block -skew-x-6 text-violet-500">.xyz </span>
       </h1>
-      <p className="text-zinc-300 text-base">
-        Read the{" "}
-        <code className="bg-zinc-800 text-zinc-300 px-2 rounded py-1 text-sm mx-1">
-          README.md
-        </code>{" "}
-        file to get started.
-      </p>
+
     </header>
   );
 }
