fe2ad8dd1ea3997a9e9ad64c18f847f263e53163
My EVM <yo@myevm.org>
2025-03-22 08:27:36 +0000

    .

--- src/pages/ClaimPage.tsx
+++ src/pages/ClaimPage.tsx
@@ -3,7 +3,7 @@
 export default function Claim() {
     return (
       <div className="text-white text-center mt-10">
-        <h1 className="text-3xl font-bold">Use XP too Claim Airdrop Allocations</h1>
+        <h1 className="text-3xl font-bold">Use XP to Claim an Airdrop Allocation</h1>
         <p className="text-sm mt-2">Coming soon...</p>
       </div>
     );
