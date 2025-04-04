b3022f0f8cd4309d99fbe0a573a63ecafbc1d9fe
Fun Fund <yo.myevm@gmail.com>
2025-04-04 14:57:13 +0000

    .

--- src/components/ComplaintPage.tsx
+++ src/components/ComplaintPage.tsx
@@ -36,24 +36,44 @@ const ComplaintPage = () => {
       <div className="mt-6">
         <h2 className="text-lg">Votes: {votes}</h2>
         <div className="flex gap-4 mt-4">
-          <button 
-            className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600" 
-            onClick={handleUpvote}
-          >
-            Upvote
-          </button>
-          <button 
-            className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600" 
-            onClick={handleDownvote}
-          >
-            Downvote
-          </button>
-          <button 
-            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600" 
-            onClick={handleAppeal}
-          >
-            Appeal
-          </button>
+          {/* Upvote button with tooltip */}
+          <div className="relative group">
+            <button 
+              className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600" 
+              onClick={handleUpvote}
+            >
+              Upvote
+            </button>
+            <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-36 p-2 text-sm text-white bg-zinc-800 border border-zinc-700 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200">
+              I Agree With This
+            </div>
+          </div>
+
+          {/* Downvote button with tooltip */}
+          <div className="relative group">
+            <button 
+              className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600" 
+              onClick={handleDownvote}
+            >
+              Downvote
+            </button>
+            <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-36 p-2 text-sm text-white bg-zinc-800 border border-zinc-700 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200">
+              I Disagree With This
+            </div>
+          </div>
+
+          {/* Appeal button with tooltip */}
+          <div className="relative group">
+            <button 
+              className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600" 
+              onClick={handleAppeal}
+            >
+              Appeal
+            </button>
+            <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-48 p-2 text-sm text-white bg-zinc-800 border border-zinc-700 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200">
+              Submit to DAO for removal (coming soon)
+            </div>
+          </div>
         </div>
       </div>
 
